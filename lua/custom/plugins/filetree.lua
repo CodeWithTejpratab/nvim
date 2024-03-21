cb8ba0258679083447547b0ea753c3b36e232ce7
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2024-03-20 21:58:22 -0400

    Add files via upload

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,26 @@
+vim.opt.termguicolors = true
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {
+      sort = {
+        sorter = 'case_sensitive',
+      },
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+    }
+  end,
+}
